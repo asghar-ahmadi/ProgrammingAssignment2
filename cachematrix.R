0ef8b2a5bbcd2a54f635e4a3c6d372929917395b
Asghar Ahmadi <asghar.ahmadi@myacu.edu.au>
2021-12-19 12:32:18 +1100

    Update assignment

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,54 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+
+
+
+
+
+makeCacheMatrix <- function( m = matrix() ) {
+
+
+    i <- NULL
+
+    set <- function( matrix ) {
+            m <<- matrix
+            i <<- NULL
+    }
+
+   
+    get <- function() {
+    	m
+    }
+
+    setInverse <- function(inverse) {
+        i <<- inverse
+    }
+
+    getInverse <- function() {
+        i
+    }
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+}
+
+
+cacheSolve <- function(x, ...) {
+
+    m <- x$getInverse()
+
+    if( !is.null(m) ) {
+            message("getting cached data")
+            return(m)
+    }
+
+    data <- x$get()
+
+    m <- solve(data) %*% data
+
+    x$setInverse(m)
+
+    m
+}
